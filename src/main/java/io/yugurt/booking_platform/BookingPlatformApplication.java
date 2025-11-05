d180da1bc3df753968eb773391f1138e8d64825b
hyungi <hy46@tardistechnology.com>
2025-11-05 14:58:26 +0900

    init

---
+++ src/main/java/io/yugurt/booking_platform/BookingPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.yugurt.booking_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yugurt/booking_platform/BookingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yugurt.booking_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
