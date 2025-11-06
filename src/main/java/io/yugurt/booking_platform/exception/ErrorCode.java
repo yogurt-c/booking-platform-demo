b007d46d0b14a0425d8e3888532f597aa46baeed
hyungi <hy46@tardistechnology.com>
2025-11-06 16:50:39 +0900

    Change: 에러 핸들링 로직 개선

--- src/main/java/io/yugurt/booking_platform/exception/ErrorCode.java
+++ src/main/java/io/yugurt/booking_platform/exception/ErrorCode.java
@@ -2,12 +2,14 @@ package io.yugurt.booking_platform.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    ACCOM_NOT_FOUND("해당 숙박 업소를 찾을 수 없습니다.");
+    ACCOM_NOT_FOUND("해당 숙박 업소를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String description;
+    private final HttpStatus status;
 }
