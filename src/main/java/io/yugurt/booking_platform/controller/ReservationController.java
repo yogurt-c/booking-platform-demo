ff579e164a05b85bff1ced23f784f918a99b4f8d
hyungi <hy46@tardistechnology.com>
2025-11-07 10:28:43 +0900

    Add: 예약 취소

--- src/main/java/io/yugurt/booking_platform/controller/ReservationController.java
+++ src/main/java/io/yugurt/booking_platform/controller/ReservationController.java
@@ -7,12 +7,15 @@ import io.yugurt.booking_platform.service.ReservationService;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,5 +41,12 @@ public class ReservationController {
     public List<ReservationDetailResponse> getMyReservations(@RequestParam String guestPhone) {
 
         return reservationService.getMyReservations(guestPhone);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void cancelReservation(@PathVariable Long id) {
+
+        reservationService.cancelReservation(id);
     }
 }
